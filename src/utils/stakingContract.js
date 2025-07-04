405897f9301ce7e4b6acb6358c95b04134ab7355
ellycit.international.dao.1.10@gmail.com <131910208+EllyCitDao112@users.noreply.github.com>
2025-07-04 14:02:08 +0300

    Update stakingContract.js
    import { web3Provider } from './walletConnectProvider';
    import abi from '../abi.json';
    
    const CONTRACT_ADDRESS = "0x36a0a9b083f68f20E7C3B930CFd5b3900845b94E";
    
    export function getStakingContract() {
      return new web3Provider.Contract(CONTRACT_ADDRESS, abi);
    }

--- src/utils/stakingContract.js
+++ src/utils/stakingContract.js
@@ -1,1 +1,8 @@
+import { web3Provider } from './walletConnectProvider';
+import abi from '../abi.json';
 
+const CONTRACT_ADDRESS = "0x36a0a9b083f68f20E7C3B930CFd5b3900845b94E";
+
+export function getStakingContract() {
+  return new web3Provider.Contract(CONTRACT_ADDRESS, abi);
+}
