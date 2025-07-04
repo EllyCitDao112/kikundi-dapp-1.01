45e6042e90d7ed2ab718bf53f90368275a58d9a7
ellycit.international.dao.1.10@gmail.com <131910208+EllyCitDao112@users.noreply.github.com>
2025-07-04 14:14:59 +0300

    Update WalletConnectButton.js
    export function WalletConnectButton() {
      const btn = document.createElement('button');
      btn.innerText = 'Connect WalletConnect';
      btn.onclick = async () => {
        console.log('WalletConnect flow would start here');
      };
      document.body.appendChild(btn);
    }

--- src/components/WalletConnectButton.js
+++ src/components/WalletConnectButton.js
@@ -1,1 +1,8 @@
-
+export function WalletConnectButton() {
+  const btn = document.createElement('button');
+  btn.innerText = 'Connect WalletConnect';
+  btn.onclick = async () => {
+    console.log('WalletConnect flow would start here');
+  };
+  document.body.appendChild(btn);
+}
