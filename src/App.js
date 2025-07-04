b9b1c297b9452b2d9d6790d5c9e5fcc52054c513
ellycit.international.dao.1.10@gmail.com <131910208+EllyCitDao112@users.noreply.github.com>
2025-07-04 14:23:02 +0300

    Update App.js
    import { initWalletConnect } from './utils/walletConnectProvider';
    import { loadStakingDashboard } from './components/StakingDashboard';
    
    export default async function App() {
      await initWalletConnect();
      loadStakingDashboard();
    }

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
 
+import { initWalletConnect } from './utils/walletConnectProvider';
+import { loadStakingDashboard } from './components/StakingDashboard';
+
+export default async function App() {
+  await initWalletConnect();
+  loadStakingDashboard();
+}
