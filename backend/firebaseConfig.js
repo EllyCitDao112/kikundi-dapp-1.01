a165db4440d2bb25d39289142070bba2ab279416
ellycit.international.dao.1.10@gmail.com <131910208+EllyCitDao112@users.noreply.github.com>
2025-07-04 20:25:12 +0300

    Create firebaseConfig.js
    // Firebase config for Kikundi App
    export const firebaseConfig = {
      apiKey: "AIzaSyXXXXXX-YourKey",
      authDomain: "kikundi-app.firebaseapp.com",
      projectId: "kikundi-app",
      storageBucket: "kikundi-app.appspot.com",
      messagingSenderId: "1234567890",
      appId: "1:1234567890:web:abcdefg"
    };

---
+++ backend/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Firebase config for Kikundi App
+export const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXX-YourKey",
+  authDomain: "kikundi-app.firebaseapp.com",
+  projectId: "kikundi-app",
+  storageBucket: "kikundi-app.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdefg"
+};
